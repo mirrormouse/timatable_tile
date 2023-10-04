89c6cef80f2c04acc551c67ca24645712eef4152
mirrormouse <kagamidokei@gmail.com>
2023-10-05 00:09:04 +0900

    initial commit

--- editScripts.js
+++
@@ -1,9 +0,0 @@
-function submitEdit() {
-    const subject = document.getElementById('subjectInput').value;
-    const color = document.getElementById('colorSelect').value;
-
-    localStorage.setItem('newSubject', subject);
-    localStorage.setItem('newColor', color);
-
-    window.close(); // 編集ページを閉じる
-}
--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
+let currentCell = null;
+
 function editCell(cell) {
-    localStorage.setItem('editingCellIndex', Array.from(cell.parentNode.children).indexOf(cell)); // セルのインデックスを保存
-    window.open('edit.html', '_blank', 'width=400,height=200');
+    currentCell = cell; // 編集中のセルを保存
+    document.getElementById('subjectInput').value = cell.textContent;
+    document.getElementById('colorSelect').value = cell.style.backgroundColor || "#FFFFFF";
+    document.querySelector('.edit-area').style.display = 'block'; // 編集エリアを表示
 }
 
-// ページ読み込み時、またはウィンドウフォーカス時にlocalStorageの変更を確認する
-window.addEventListener('focus', () => {
-    const newSubject = localStorage.getItem('newSubject');
-    const newColor = localStorage.getItem('newColor');
-    const cellIndex = parseInt(localStorage.getItem('editingCellIndex'));
+function submitEdit() {
+    const subject = document.getElementById('subjectInput').value;
+    const color = document.getElementById('colorSelect').value;
 
-    if (newSubject !== null && newColor !== null && !isNaN(cellIndex)) {
-        document.querySelector(`.row:nth-child(${cellIndex + 2}) > div:nth-child(${cellIndex + 1})`).textContent = newSubject;
-        document.querySelector(`.row:nth-child(${cellIndex + 2}) > div:nth-child(${cellIndex + 1})`).style.backgroundColor = newColor;
-        localStorage.removeItem('newSubject'); // 使用後にデータをクリア
-        localStorage.removeItem('newColor'); // 使用後にデータをクリア
+    if (currentCell) {
+        currentCell.textContent = subject;
+        currentCell.style.backgroundColor = color;
+        document.querySelector('.edit-area').style.display = 'none'; // 編集エリアを非表示にする
     }
-});
+}
